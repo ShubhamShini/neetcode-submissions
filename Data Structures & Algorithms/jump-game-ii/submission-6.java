4121fc9351af1f52ecf4f2aa2e0cadc48f9f714c
ShubhamShini <85511861+ShubhamShini@users.noreply.github.com>
2026-09-01 00:16:04 +0530

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length == 1) return 0;
+        int jump =1;
+        int maxtill =0;
+        int a = nums[0];
+
+        for(int i=0;i<nums.length;i++) {
+            maxtill = Math.max(maxtill, i + nums[i]);
+            if(i == a) {
+                jump++;
+                a = maxtill;
+            }
+            if(a >= nums.length-1) return jump;
+        }
+        return jump;
+        
+    }
+}
